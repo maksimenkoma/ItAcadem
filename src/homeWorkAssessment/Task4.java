d28a4b57df27bad04954efa74457de8104f76f76
maksimenkoma <maksimenkoma@tut.by>
2021-10-27 20:13:46 +0300

    Task4Fix

--- src/homeWorkAssessment/Task4.java
+++ src/homeWorkAssessment/Task4.java
@@ -5,7 +5,7 @@
 //        вверху и снизу от пересечения диагоналей находятся только положительные числа;
 //        слева и справа от пересечения диагоналей находятся только отрицательные числа;
 //
-//        Вывести массив на экран.
+//        Вывести массив ына экран.
 //
 //        Найти сумму всех элементов
 //        Найти среднее арифметическое всех элементов, которые больше суммы всех элементов
@@ -19,7 +19,6 @@ public class Task4 {
         int n = 7;
         int j = 1;
         int b = 0;
-        int count = 0;
         int count2 = 0;
         int sum = 0;
         int sum2 = 0;
@@ -86,8 +85,13 @@ public class Task4 {
             for (int array3 : array2) {
 
                 sum += array3;
-                count++;
-                if (array3 > -3) {
+
+            }
+
+        }
+        for (int[] array2 : array) {
+            for (int array3 : array2) {
+                if (array3 > sum) {
                     count2++;
                     sum2 += array3;
 
@@ -96,8 +100,10 @@ public class Task4 {
             }
 
         }
+
+
         double arithmeticMean = (double) sum2 / count2;
         System.out.println("Сумма всех элементов: " +sum);
-        System.out.println("Cреднее арифметическое всех элементов больше -3: "+arithmeticMean);
+        System.out.println("Cреднeе арифметическое всех элементов больше " +sum+": "+arithmeticMean);
     }
 }
